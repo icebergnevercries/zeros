91227424ca4c58a5675699f333f6f19c99e19cba
icebergnevercries <denislosik@mail.ru>
2018-09-26 20:34:42 +0300

    implementation function, which calculate how many zeros in the end of factorial of number

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 module.exports = function getZerosCount(number) {
-  let countzeros = 0, degreeOfNum5 = 1, restOfNum = 0;
+  const arrayForBase = [2, 5];
+  let arrayForZeros = [];
 
-  while(number >= Math.pow(5, degreeOfNum5 + 1)){
-    degreeOfNum5++;
-  }
-  
-  for(i = 4; i > 0; i--){
-    if(i * Math.pow(5, degreeOfNum5) < number){
-      restOfNum = i;
-      break;
-    }
-  }
-  for(i = degreeOfNum5; i > 0; i--){
-    countzeros += restOfNum*Math.pow(5, degreeOfNum5 - i);
-  }
-  
-  let buff = 0;
-  for(let i = Math.pow(5, degreeOfNum5)*restOfNum + 5; i <= number; i+=5)
+  for(let i = 0, len = arrayForBase.length, buff = 0, degreeCount = 1; i < len; i++)
   {
-    buff = i;
-    while(buff % 5 == 0)
-    {
-      countzeros++;
-      buff = buff / 5;
+    while(number > Math.pow(arrayForBase[i], degreeCount)){
+      buff += Math.floor(number / Math.pow(arrayForBase[i], degreeCount));
+      degreeCount++;
     }
-  } 
+    arrayForZeros.push(buff);
+    buff = 0; degreeCount = 1;
+  }
 
-  return countzeros;
+  return (arrayForZeros[0] < arrayForZeros[1]) ? arrayForZeros[0] : arrayForZeros[1];
 }
